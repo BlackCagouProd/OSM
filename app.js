d63034efb7eaa5a4e64b4c903b02369362317ec5
Clement Wahaga <c.wahaga@cci-formation.nc>
2021-12-01 04:15:03 +0100

    add files

---
+++ app.js
@@ -0,0 +1,5 @@
+var map = L.map('carte').setView([-22.2642742, 166.4098471], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
